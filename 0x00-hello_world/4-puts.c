522b470dc158d9fe05a2fc4357b7e18c8f1e7607
SteveieBozz <kusimensahsteve26@gmail.com>
2022-12-11 17:37:59 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * main - Entry
+ * Return: Always 0
  */
 
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual ppuzzle");
+	puts("\"Programming is like building a multilingual ppuzzle");
 		return(0);
 }
