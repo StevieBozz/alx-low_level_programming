e26fe1fba57cc64e4f2d38b73ea65fdd0db4c73b
SteveieBozz <kusimensahsteve26@gmail.com>
2022-12-11 17:59:07 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry
  * Return: Always 0
@@ -6,5 +6,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual ppuzzle");
-		return (0);
+	return (0);
 }
