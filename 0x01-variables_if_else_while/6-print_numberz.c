433f16723b44c967a757c286d69c85fe66c85210
SteveieBozz <kusimensahsteve26@gmail.com>
2023-02-12 08:46:46 -0800

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - prints single digit in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int hel;
+
+	for (hel =0; hel < 10; hel++)
+		putchar((hel % 10) + '0');
+
+	putchar('\n')
+
+	return (0);
+}
